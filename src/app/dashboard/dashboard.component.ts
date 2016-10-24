b259ed891b55747e841153317b218be6470e37d5
Alexander <oleksandr.dudko@customertimes.com>
2016-10-24 18:35:01 +0300

    added remove button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,7 +24,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     public af: AngularFire
   ) { }
 
-  ngOnInit() {
+  ngOnInit() { debugger
     this.af.auth.take(1).subscribe((user: FirebaseAuthState) => {
       this.records = this.af.database.list(`/account/${user.uid}/history`);
       this.list = this.records.map(list => list.reverse());
@@ -47,7 +47,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit() { debugger
     this.ctgInput.onBlur.debounceTime(120).subscribe((e) => {
       this.ctgList.nativeElement.style.display = 'none';
     });
